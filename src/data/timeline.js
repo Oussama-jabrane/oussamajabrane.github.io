3cf77caf6bf4f26e3467dd771fb783af3aa9ae2a
Oussama Jabrane <ojabrane43@gmail.com>
2023-08-28 19:31:00 +0100

    Correcting typos

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,18 +3,18 @@ export default [
         year: 'Today',
         title: 'Software Engineering Student',
         duration: '< 1 year',
-        details: 'I am currently in the freshman year in Sichuan University, for the Software Engineering major. I am expecting to graduate by July 2027.'
+        details: 'I am presently a freshman at Sichuan University studying Software Engineering. I intend to graduate in July 2027.'
     },
     {
         year: '2021-2022',
         title: 'Expanding my knowledge',
         duration: '1 year',
-        details: 'Started working and sharpening my programming and coding skills, by learning Python language and JavaScript, and made the decision that I should pursue software engineering and coding full-time as an every-day job in the future because it matches my passion.'
+        details: 'Started working and improving my programming and coding abilities by learning Python and JavaScript, and decided that in the future, I should go into software engineering and coding field ffull-time as a career because it fits my interest.'
     },
     {
         year: '2020',
         title: 'Wrote my first line of code',
         duration: 'The Beginning',
-        details: 'I wrote my first line of real code with HTML that started this journey into digital craftsmanship that I never could\'ve imagined, especially as someone who never saw themselves as anything other than an architect.'
+        details: 'I typed my first line of actual code in HTML, which sent me on a path into digital craftsmanship that I could never have envisioned, especially as someone who had never considered themselves to be anything other than an architect.'
     },
 ]
